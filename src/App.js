fb124a10ce2109fc21ee0d2d175dc2e3ed0e08ec
Nicholas Aiello <nikko.aiello@gmail.com>
2017-10-25 10:33:12 -0500

    implement empty state for ticker

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import './App.css';
 
 /* */
 
-const AppTicker = ({ symbol, price, open, volume, refreshed }) => {
+const AppTicker = ({ query, symbol, price, open, volume, refreshed }) => {
 
   if (!symbol) {
     return (
@@ -31,35 +31,37 @@ const AppTicker = ({ symbol, price, open, volume, refreshed }) => {
     );
   }
 
-  return (
-    <div className="ticker">
-      <h4>{symbol.toUpperCase()}</h4>
-      <figure>
-        <label>last tick:</label>
-        <figcaption>
-          ${parseFloat(price).toLocaleString('en')}
-        </figcaption>
-      </figure>
-      <figure>
-        <label>open:</label>
-        <figcaption>
-          ${parseFloat(open).toLocaleString('en')}
-        </figcaption>
-      </figure>
-      <figure>
-        <label>volume:</label>
-        <figcaption>
-          {parseInt(volume).toLocaleString('en')}
-        </figcaption>
-      </figure>
-      <figure>
-        <label>updated:</label>
-        <figcaption>
-          {new Date(refreshed).toLocaleDateString()}
-        </figcaption>
-      </figure>
-    </div>
-  );
+  return (query.charAt(0) !== '$'
+    ? <div className="ticker">
+        <h4>{symbol.toUpperCase()}</h4>
+      </div>
+    : <div className="ticker">
+        <h4>{symbol.toUpperCase()}</h4>
+        <figure>
+          <label>last tick:</label>
+          <figcaption>
+            ${parseFloat(price).toLocaleString('en')}
+          </figcaption>
+        </figure>
+        <figure>
+          <label>open:</label>
+          <figcaption>
+            ${parseFloat(open).toLocaleString('en')}
+          </figcaption>
+        </figure>
+        <figure>
+          <label>volume:</label>
+          <figcaption>
+            {parseInt(volume).toLocaleString('en')}
+          </figcaption>
+        </figure>
+        <figure>
+          <label>updated:</label>
+          <figcaption>
+            {new Date(refreshed).toLocaleDateString()}
+          </figcaption>
+        </figure>
+      </div>);
 
 };
 
@@ -113,7 +115,7 @@ class App extends Component {
           query={query}
           fltr={fltr}
           dispatch={dispatch} />
-        <AppTicker {...meta} />
+        <AppTicker query={query} {...meta} />
         <AppSyncProgressBar
           query={query}
           enabled={autoSync}
--- src/constants/Env.js
+++ src/constants/Env.js
@@ -1,5 +1,5 @@
 import * as env from '../.env.local.json';
 
 export const ENV = env.ENV;
-export const BASE_API_URL = env.BASE_API_URL;
+export const BASE_API_URL = env.BASE_API_URL; // "https://nodejs-squawk-some.herokuapp.com";
 export const DEFAULT_SYNC_INTERVAL = env.DEFAULT_SYNC_INTERVAL;
